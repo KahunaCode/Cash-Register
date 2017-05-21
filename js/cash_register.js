2b7deacd2f45d64641cb5442bea9e0209400c73e
James Moore <jameslmoore@gmail.com>
2017-05-20 15:22:03 -1000

    7 8 divide works

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,7 +1,7 @@
 
 
 var memory = [];
-var operator = "divide";
+var operator =[];
 var display;
 console.log(memory);
 
@@ -12,12 +12,40 @@ button7.addEventListener("click", function(){
   document.getElementById("display").innerHTML="7";
 });
 
+var button8 = document.querySelector("#eight");
+//push to memory
+button8.addEventListener("click", function(){
+  memory.push(8);
+  document.getElementById("display").innerHTML="8";
+  console.log(memory)
+});
+
+var buttonDivide = document.querySelector("#divide");
+buttonDivide.addEventListener("click", function(){
+  operator = "/";
+  document.getElementById("display").innerHTML = "/";
+  console.log(operator);
+});
+
+var buttonEquals = document.querySelector("#equals");
+  buttonEquals.addEventListener("click", function(){
+    if (operator === "/"){
+      var total = memory[0]/memory[1];
+      console.log(total);
+      document.getElementById("display").innerHTML = total;
+    }
+
+  });
+
+
 //udpdate display
 console.log(memory);
 
 
-
-
+// total =
+//   if operator === "/"{
+//     memory[0]/memory[1]
+//   }
 
 
 
