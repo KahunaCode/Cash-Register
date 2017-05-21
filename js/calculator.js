317c9b116afb8878989bba894d4fb7ec94342e55
James Moore <jameslmoore@gmail.com>
2017-05-20 14:28:59 -1000

    calc rows and formatting

---
+++ js/calculator.js
@@ -0,0 +1,85 @@
+
+var calculatorModule = function(){
+
+  var memory = 0;
+  var total = 0;
+
+  function load(num){
+    if(typeof num === "number"){
+
+    total = num;
+    return total;
+  }
+  else{
+    throw new Error("not a number");
+  }
+  }
+
+  function getTotal(){
+    return total;
+   }
+
+  function add(num){
+    if(typeof num === "number"){
+    total += num;
+  }else
+  {
+    throw new Error("not a number");
+  }
+
+  }
+
+  function subtract(num){
+    if(typeof num === "number"){
+      total -= num;
+  }
+  else
+    {throw new Error("not a number");}
+}
+
+  function multiply(num){
+    if(typeof num === "number"){
+    total *= num;
+    }
+    else{
+      throw new Error("not a number");
+    }
+  }
+
+  function divide(num){
+    if(typeof num === "number"){
+    total /= num;
+    }
+    else{
+      throw new Error("not a number")
+    }
+  }
+
+  function recallMemory(){
+    return memory;
+  }
+
+  function saveMemory(){
+    memory = total;
+    return memory;
+  }
+
+  function clearMemory(){
+    memory = 0;
+    return memory;
+  }
+
+
+
+   return {
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory
+   };
+};
